3ef90ce79dd7927c2a9adeb37f572c247f28185c
AbdulJH <abdul.chs21@gmail.com>
2025-10-07 04:10:55 -0500

    when user registers they are now stored in the db

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import psycopg2
+def insert_user(username, password):
+    conn = psycopg2.connect(
+        dbname="ics499db",
+        user="postgres",
+        password="",
+        host="localhost"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO users (username, password) VALUES (%s, %s)",
+        (username, password)
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -7,6 +24,7 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        insert_user(username, password)
         print(f"Username: {username}, Password: {password}")
         return render_template("register.html")
     return render_template("register.html")
