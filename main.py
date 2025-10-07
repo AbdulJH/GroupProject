1f577e92d0fdb9565ba1c3bafc29d80c73ff9008
AbdulJH <abdul.chs21@gmail.com>
2025-10-07 15:22:15 -0500

    user is now able to register an account, cant make account with same username, created check_user method for that and check valid login used in the login function

--- main.py
+++ main.py
@@ -15,13 +15,7 @@ def insert_user(username, password):
     cur.close()
     conn.close()
 
-# create a method username_taken to check dataase to see if username is already taken. if it is return true if its not return false 
-# then in  register method we do if !username_taken(username):
-# insert_user() else we return usernaem taken message or popup
-
-
-
-def username_taken(username):
+def check_user(username):
     conn = psycopg2.connect(
         dbname="ics499db",
         user="postgres",
@@ -30,13 +24,35 @@ def username_taken(username):
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM users WHERE username = %s",
-        (username,)
-    )
-    result = cur.fetchone()
+        "SELECT 1 FROM users WHERE username = %s",
+        (username,))
+    user = cur.fetchone()
     cur.close()
     conn.close()
-    return result is not None
+    return user is not None
+
+def valid_login(username, password):
+    conn = psycopg2.connect(
+        dbname="ics499db",
+        user="postgres",
+        password="",
+        host="localhost"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT 1 FROM users WHERE username = %s AND password = %s",
+        (username, password))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+    return user is not None
+
+    
+# create a method username_taken to check dataase to see if username is already taken. if it is return true if its not return false 
+# then in  register method we do if !username_taken(username):
+# insert_user() else we return usernaem taken message or popup
+
+
 
 from flask import Flask, request, render_template
 
@@ -47,10 +63,25 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        if check_user(username):
+            return render_template("register.html", username_taken=True)
         insert_user(username, password)
         print(f"Username: {username}, Password: {password}")
         return render_template("register.html")
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["login-username"]
+        password = request.form["login-password"]
+        if not valid_login(username, password):
+            return render_template("login.html", login_failed=True)
+        print("Login Successful")
+        return "Login successful!"
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
